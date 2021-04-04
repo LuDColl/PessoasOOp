0b4209f0b18d11099f4bc1558cfe2ccd5d3fadd7
Lucas Guimarães de Oliveira <lucas.guimaraes.lc@gmail.com>
2021-04-04 18:18:22 -0300

    Criei um While para a digitação

--- Pessoas OOp/src/App.java
+++ Pessoas OOp/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        while (true) {
+            System.out.println("Digite os dados da pessoa:");
+            
+        }
     }
 }
